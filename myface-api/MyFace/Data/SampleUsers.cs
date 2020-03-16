e29f1656618622cd5dcf4eba0468d0aff5c7a73d
Beatrice <beatrice.opara@softwire.com>
2020-03-16 15:49:43 +0000

    Added password to database
    added hash password and salt property to user.cs
    hard coded info into sampleusers.cs

--- myface-api/MyFace/Data/SampleUsers.cs
+++ myface-api/MyFace/Data/SampleUsers.cs
@@ -127,6 +127,8 @@ namespace MyFace.Data
                 Email = _data[index][3],
                 ProfileImageUrl = ImageGenerator.GetProfileImage(_data[index][2]),
                 CoverImageUrl = ImageGenerator.GetCoverImage(index),
+                HashedPassword = "sugar",
+                Salt = "1234abcd"
             };
         }
     }
